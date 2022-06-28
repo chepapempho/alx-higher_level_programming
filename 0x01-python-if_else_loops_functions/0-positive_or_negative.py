b56079d806b8274433314537a661de88db48258f
chepapempho <chepapempho@gmail.com>
2022-06-28 12:17:04 +0000

    first commit

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -9,7 +9,7 @@ if number < 0:
 # print statements 
 if last_digit>5:
     print("Last digit of {number:d} is {last_digit:d} and is greater than 5")
-elseif last_digit!=0 and last_digit<6:
+elif last_digit!=0 and last_digit<6:
     print("Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
 else:
     print("Last digit of {number:d} is {last_digit:d} is 0")
