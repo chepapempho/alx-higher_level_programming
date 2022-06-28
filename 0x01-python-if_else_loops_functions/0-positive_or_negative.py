e3e46e0b76318a80ba080b04784a19266fbe355b
chepapempho <chepapempho@gmail.com>
2022-06-28 12:24:15 +0000

    fix + task 1

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,14 +2,9 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
-last_digit = number%10
-#set sign for last digit
-if number < 0:
-    last_digit = last_digit*(-1)
-# print statements 
-if last_digit>5:
-    print("Last digit of {number:d} is {last_digit:d} and is greater than 5")
-elif last_digit!=0 and last_digit<6:
-    print("Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
+if number > 0:
+    print(f"{number:d} is positive
+elif number == 0:
+    print(f"{number:d} is zero")
 else:
-    print("Last digit of {number:d} is {last_digit:d} is 0")
+    print(f"{number:d} is negetive")
---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+last_digit = number % 10
+#set sign for last digit
+if number < 0:
+    last_digit = last_digit*(-1)
+# print statements 
+if last_digit > 5:
+    print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
+elif last_digit != 0 and last_digit < 6:
+    print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number:d} is {last_digit:d} is 0")
